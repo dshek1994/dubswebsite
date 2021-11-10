6baa749c7a0b33e20b3798bcb5a883097b443a70
Daniel S <dshekari@icloud.com>
2021-11-10 01:25:06 -0600

    Implemented Dub's navbar into website

--- dubs-website/src/App.js
+++ dubs-website/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import Navbar from './components/navbar/navbar';
 // import Store from './pages/navbar/Store';
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import navbar from './components/navbar/navbar';
@@ -9,7 +10,9 @@ import React from "react";
 function App() {
   return (
     <div class="container">
-      
+      <div>
+        <Navbar />
+      </div>
     </div>
   );
 }
